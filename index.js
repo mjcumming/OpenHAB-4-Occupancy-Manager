e7f98336ea6df06eb819d64749f1a1f2f0e9064e
Michael Cumming <mike@4831.com>
2023-11-27 09:46:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const OccupancyManager = require('./occupancyManager');
 
-console.log("Occupancy Manager Starting");
-
-console.log(typeof OccupancyManager);
-
 module.exports = {
   OccupancyManager: OccupancyManager
 };
